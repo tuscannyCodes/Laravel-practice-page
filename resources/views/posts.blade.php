10c36bc4c66e64bc6eb1991b4962eae2344733ba
Tuscanny Polk <tuscannypolk@gmail.com>
2022-04-12 21:15:19 -0400

    first commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TESTING LARAVEL_START</title>
+    <link rel="stylesheet" href="app.css">
+</head>
+
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+        <p>
+            This is a practice page made using Laravel. This page will be set up as a 'test blog' and will be used primarily for testing purposes.
+        </p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+        <p>
+            This is a practice page made using Laravel. This page will be set up as a 'test blog' and will be used primarily for testing purposes.
+        </p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+        <p>
+            This is a practice page made using Laravel. This page will be set up as a 'test blog' and will be used primarily for testing purposes.
+        </p>
+    </article>
+</body>
+
+</html>
